4375648b9a6b0bc770aea6d65c2946bde4640164
Ahmad Alnashoukati <ahmad.alnashoukati@vianeos.com>
2025-04-27 10:38:47 +0400

    Initial Commit

---
+++ InstagramFeedClone/InstagramFeedCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramFeedCloneApp.swift
+//  InstagramFeedClone
+//
+//  Created by Ahmad on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramFeedCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
